6f53fca0b0c723ab3cbcc1ffd8a2b4672925c6f9
Gabriele <gabriele@pt-oliveto.station>
2025-12-07 12:19:43 +0100

    beginning to add wildcards

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -3,9 +3,17 @@ def parse_input(rule):
     with open(rule) as f:
         lines = f.readlines()
         for line in lines:
-            stripped_line = line.strip()                                                        # strip lines to work with strings
+            stripped_line = line.strip()                                                       # strip lines to work with strings
             stripped_lines.append(stripped_line)
     return [lines, stripped_lines]
+    
+def track_dependencies(rules_paths, lowerBound):
+    for idx in range(len(rules_paths)):                                              # scan rulesDir
+        rule_path = rules_paths[idx]
+        rule_basename = rules_basenames[idx]
+        print(f'[log: Tracking dependencies {rule_path} ]')
+        lines, stripped_lines = parse_input(rule_path)
+        input_output_log_dic = apply_changes(stripped_lines, category, oldPattern = oldPattern, newPattern = newPattern, oldName = oldName, newName = newName)
     
     
 def apply_changes(stripped_lines, category, **diffs):
@@ -19,9 +27,8 @@ def apply_changes(stripped_lines, category, **diffs):
             range_idxs = range(start_idx, end_idx)                                              # define range of lines where filenames are
             for idx in range_idxs:                                                              # iterate
                 # modify lines and store in dictionary
-                modified_line = modify_line(stripped_lines[idx].replace('\"',''), category, diffs)
-                # todo: keep track of variables being assigned
-                input_output_log_dic[idx] = "\t" + '\"' + modified_line + '\"' + "\n"
+                modified_line = modify_line(stripped_lines[idx], category, diffs)
+                input_output_log_dic[idx] = "\t" + "\t" + modified_line + "\n"
         else:
             pass
     return input_output_log_dic
@@ -36,7 +43,10 @@ def modify_line(string, category, diffs):
     elif category == "remove_wildcard":
         # check for wildcard
         for diff in diffs["oldPattern"]:
-            string_new = string.replace(diff, "") 
+            string_new = string.replace(diff, "")
+        if string != string_new:
+            print(f'[log: << {string} ]')
+            print(f'[log: >> {string_new} ]')
         return string_new
     elif category == "modify_wildcard":
         # check for wildcard
@@ -46,27 +56,33 @@ def modify_line(string, category, diffs):
             for idx in range(len(diffs["oldPattern"])):
                 old = diffs["oldPattern"][idx]
                 new = diffs["newPattern"][idx]
-                string_new = string.replace(old, new) 
+                string_new = string.replace(old, new)
+            if string != string_new:
+                print(f'[log: << {string} ]')
+                print(f'[log: >> {string_new} ]')
             return string_new
         else:
             raise ValueError(f"oldPattern [{len(olds)} elements] and newPattern [{len(news)} elements] have different lengths")
     elif category == "modify_filename":
         old = diffs["oldName"]
         new = diffs["newName"]
-        string_new = string.replace(old, new) 
+        string_new = string.replace(old, new)
+        if string != string_new:
+            print(f'[log: << {string} ]')
+            print(f'[log: >> {string_new} ]')
         return string_new
     
 def update_lines(lines, input_output_log_dic):
-    for idx in range(len(lines)):                                                               # for each line
+    lines_copy = lines.copy()
+    for idx in range(len(lines_copy)):                                                          # for each line
         if idx in list(input_output_log_dic.keys()):                                            # if the line is one we modified
-            lines[idx] = input_output_log_dic[idx]                                              # change it
+            lines_copy[idx] = input_output_log_dic[idx]                                         # change it
         else:
-            lines[idx] = lines[idx]
-    return lines
+            lines_copy[idx] = lines[idx]
+    return lines_copy
     
-def write_rule(lines, outputDir, ruleName):                                             
+def write_rule(lines_updated, outputDir, ruleName):                                             
     outputPath = outputDir + ruleName                                                           # define path to write new rule
-    print(f'[log: Writing {outputPath} ]')
     with open(outputPath, "w") as f:                                                       
-        for line in lines:
+        for line in lines_updated:
             f.write(line)
--- scripts/main.py
+++ scripts/main.py
@@ -3,13 +3,15 @@ import argparse
 from helpers import * 
 
 parser = argparse.ArgumentParser()  
-parser.add_argument('--rulesDir',   dest='rulesDir',                required=True, type=str, help='Add rulesDir')
-parser.add_argument('--outputDir',  dest='outputDir',               required=True, type=str, help='Add outputDir')
-parser.add_argument('--change',     dest='change',                                 type=str, help='Add change')
-parser.add_argument('--oldPattern', dest='oldPattern', nargs = "*",                type=str, help='Add oldPattern')
-parser.add_argument('--newPattern', dest='newPattern', nargs = "*",                type=str, help='Add newPattern')
-parser.add_argument('--oldName',    dest='oldName',    nargs = "?",                type=str, help='Add oldName')
-parser.add_argument('--newName',    dest='newName',    nargs = "?",                type=str, help='Add newName')
+parser.add_argument('--rulesDir',      dest='rulesDir',                   required=True,    type=str,    help='Add rulesDir')
+parser.add_argument('--outputDir',     dest='outputDir',                  required=True,    type=str,    help='Add outputDir')
+parser.add_argument('--change',        dest='change',                                       type=str,    help='Add change')
+parser.add_argument('--oldPattern',    dest='oldPattern',    nargs = "*",                   type=str,    help='Add oldPattern')
+parser.add_argument('--newPattern',    dest='newPattern',    nargs = "*",                   type=str,    help='Add newPattern')
+parser.add_argument('--oldName',       dest='oldName',       nargs = "?",                   type=str,    help='Add oldName')
+parser.add_argument('--newName',       dest='newName',       nargs = "?",                   type=str,    help='Add newName')
+parser.add_argument('--lowerBound',    dest='lowerBound',    nargs = "?",                   type=str,    help='Add newName')
+
 
 args = parser.parse_args()
 
@@ -21,12 +23,25 @@ oldPattern = [] if args.oldPattern == None else args.oldPattern
 newPattern = [] if args.newPattern == None else args.newPattern
 oldName = [] if args.oldName == None else args.oldName
 newName = [] if args.newName == None else args.newName
+lowerBound = [] if args.lowerBound == None else args.lowerBound
 
-for idx in range(len(rules_paths)):                                              # scan rulesDir
-    rule_path = rules_paths[idx]
-    rule_basename = rules_basenames[idx]
-    print(f'[log: Processing {rule_path} ]')
-    lines, stripped_lines = parse_input(rule_path)
-    input_output_log_dic = apply_changes(stripped_lines, category, oldPattern = oldPattern, newPattern = newPattern, oldName = oldName, newName = newName)
-    lines = update_lines(lines, input_output_log_dic)
-    write_rule(lines, args.outputDir, rule_basename)
+if category != "add_wildcard":
+    for idx in range(len(rules_paths)):                                              # scan rulesDir
+        rule_path = rules_paths[idx]
+        rule_basename = rules_basenames[idx]
+        print(f'[log: Processing {rule_path} ]')
+        lines, stripped_lines = parse_input(rule_path)
+        input_output_log_dic = apply_changes(stripped_lines, category, oldPattern = oldPattern, newPattern = newPattern, oldName = oldName, newName = newName)
+        lines_updated = update_lines(lines, input_output_log_dic)
+        write_rule(lines_updated, args.outputDir, rule_basename)
+        
+else:
+    #dependencies = track_dependencies(rules_paths, lowerBound)
+    #for idx in range(len(dependencies)):                                              # scan rulesDir
+    #    dependency_path = dependencies[idx]
+    #    dependency_basename = ...
+    #    print(f'[log: Processing {dependency_basename} ]')
+    #    lines, stripped_lines = parse_input(rule_path)
+    #    input_output_log_dic = apply_changes(stripped_lines, category, newPattern = newPattern, lowerBound = lowerBound)
+    #    lines_updated = update_lines(lines, input_output_log_dic)
+    #    write_rule(lines_updated, args.outputDir, rule_basename)
